bfbd345c475a74661c765dea50438446cd507c58
sky-train <sklyarov.2y@gmail.com>
2024-07-12 01:33:32 +0300

    feat: remove dependency from console

--- README.md
+++ README.md
@@ -2,5 +2,19 @@
 
     abstract AppException
 
+```dart 
 
+  void print() {
+    var data = <String>[];
+    data.add('[ERROR 💀️] message: $message ${parent != null ? ' parent: ${parent?.runtimeType.toString()}' : ''}');
+    if (stackTrace != null) {
+      data.add('[STACK 🔦💀] $stackTrace');
+    }
+    for (final item in _context.entries) {
+      data.add('[CONTEXT 🤮💀️] ${item.key.toString()}, ${item.value.toString()}');
+    }
+    console.error(data.join('\n'));
+  }
+
+```
 
--- lib/exception.dart
+++ lib/exception.dart
@@ -1,7 +1,5 @@
 library;
 
-import 'package:console/console.dart';
-
 abstract class AppException implements Exception {
   abstract final String code;
   final Object? parent;
@@ -34,15 +32,4 @@ abstract class AppException implements Exception {
     return message;
   }
 
-  void print() {
-    var data = <String>[];
-    data.add('[ERROR 💀️] message: $message ${parent != null ? ' parent: ${parent?.runtimeType.toString()}' : ''}');
-    if (stackTrace != null) {
-      data.add('[STACK 🔦💀] $stackTrace');
-    }
-    for (final item in _context.entries) {
-      data.add('[CONTEXT 🤮💀️] ${item.key.toString()}, ${item.value.toString()}');
-    }
-    console.error(data.join('\n'));
-  }
 }
